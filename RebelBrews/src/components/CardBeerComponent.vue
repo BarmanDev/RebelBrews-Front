5e90e9ecf010077cd0afbe6e14f2340f45012d01
Borja <elbarmandev@gmail.com>
2024-03-03 18:35:53 +0100

    beers cards axios call component and template

---
+++ RebelBrews/src/components/CardBeerComponent.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="beer-card">
+        <div class="img-beer">
+            <img :src="beer.image_url" alt="Beer Image" v-if="beer.image_url" />
+        </div>
+        <div class="title-beer">
+            <h2>{{ beer.name }}</h2>
+        </div>
+    </div>
+   </template>
+   
+   <script setup lang="ts">
+   import { defineProps } from 'vue';
+   
+   const props = defineProps({
+    beer: Object
+   });
+   </script>
+   
+   <style scoped>
+   .beer-card {
+    border: 1px solid #ccc;
+    padding: 20px;
+    margin: 20px;
+    text-align: center;
+   }
+   .beer-card img {
+    max-width: 100%; 
+    height: auto; 
+    max-height: 200px; 
+    object-fit: cover; 
+}
+   </style>
--- RebelBrews/src/views/BeersView.vue
+++ RebelBrews/src/views/BeersView.vue
@@ -1,6 +1,33 @@
 <script setup lang="ts">
+import { ref, onMounted } from 'vue';
+import axios from 'axios';
+import BeerCard from '../components/CardBeerComponent.vue';
+
+const beers = ref([]);
+
+onMounted(async () => {
+ try {
+    const response = await axios.get('https://api.punkapi.com/v2/beers?page=1&per_page=6');
+    beers.value = response.data.filter(beer => beer.image_url);
+ } catch (error) {
+    console.error('Error fetching beer data:', error);
+ }
+});
 </script>
 
 <template>
-  <h1>Beeeeers</h1>
+ <h1>Beeeeers</h1>
+ <div class="beers-container">
+  <BeerCard v-for="beer in beers" :key="beer.id" :beer="beer" />
+</div>
+
 </template>
+
+<style scoped lang="scss">
+.beers-container {
+ display: flex;
+ flex-wrap: wrap;
+ justify-content: space-between;
+}
+
+</style>
