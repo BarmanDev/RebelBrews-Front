d13ea5699cf797427ca1f42574c02efd19a14e8f
Borja <elbarmandev@gmail.com>
2024-03-03 16:54:39 +0100

    positioning of navbar elements

--- RebelBrews/src/App.vue
+++ RebelBrews/src/App.vue
@@ -1,15 +1,16 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
+import NavbarComponent from './components/NavbarComponent.vue';
 </script>
 
 <template>
-  <header>
+  <!-- <header>
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
       </nav>
-  </header>
-
+  </header> -->
+  <NavbarComponent/>
   <RouterView />
 </template>
 
---
+++ RebelBrews/src/components/NavbarComponent.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const isMenuOpen = ref(false);
+
+
+</script>
+
+<template>
+ <nav class="navbar">
+    <div class="navbar-logo">
+      <img src="../assets/img/logo.png" alt="Icono">
+    </div>
+    <div class="navbar-links">
+      <a href="#comentarios">Promociones</a>
+      <a href="#empresa">Cervezas</a>
+      <a href="#locales">Locales</a>
+      <a href="#cerveza">Empresa</a>
+      <a id="btn-start" href="#promociones">Comienza</a>
+    </div>
+        <img class="navbar-icon" src="../assets/icons/menu-icon.svg" alt="Menu" aria-label="Toggle menu">
+
+    
+ </nav>
+
+</template>
+
+<style scoped lang="scss">
+    @import '../assets/navbar.scss'
+
+</style>
