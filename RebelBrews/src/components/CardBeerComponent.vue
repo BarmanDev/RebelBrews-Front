7fe34b266fa723ad29277ced52a96aeff9b3f4e9
Borja <elbarmandev@gmail.com>
2024-03-16 12:56:11 +0100

    api openbrewerydb

--- RebelBrews/src/components/CardBeerComponent.vue
+++ RebelBrews/src/components/CardBeerComponent.vue
@@ -4,21 +4,19 @@
             <button @click="toggleTooltip" class="info-button">Info</button>
             <button @click="addToFavorites" class="favorite-button">Favorito</button>
         </div>
-        <div class="img-beer">
-            <img :src="beer?.image_url" alt="Beer Image" v-if="beer?.image_url" />
-        </div>
         <div class="title-beer">
-            <h2>{{ beer?.name }}</h2>
-            <p class="beer-description">{{ truncateDescription(beer?.description, 150) }}</p>
+            <h2>{{ brewery.name }}</h2>
+            <p>{{ brewery.brewery_type }}</p>
+            <p>{{ brewery.street }}</p>
+            <p>{{ brewery.city }}, {{ brewery.state }} {{ brewery.postal_code }}</p>
+            <p>{{ brewery.country }}</p>
+            <p><a :href="brewery.website_url" target="_blank">Visitar sitio web</a></p>
         </div>
         <div v-if="showTooltip" class="tooltip">
-            <p>Graduación: {{ beer?.abv }}%</p>
-            <p>Amargor: {{ beer?.ibu }} IBU</p>
-            <p>Ingredientes de Malta: {{ beer?.ingredients.malt.map(malt => malt.name).join(', ') }}</p> 
-            <p>Ingredientes de Lúpulo: {{ beer?.ingredients.hops.map(hop => hop.name).join(', ') }}</p> 
-            <p>Levadura: {{ beer?.ingredients.yeast }}</p> 
+            <p>Teléfono: {{ brewery.phone }}</p>
+            <p>Latitud: {{ brewery.latitude }}</p>
+            <p>Longitud: {{ brewery.longitude }}</p>
         </div>
-
     </div>
 </template>
 
@@ -26,25 +24,20 @@
 import { defineProps, ref } from 'vue';
 
 const props = defineProps({
-    beer: Object
+    brewery: Object
 });
 
 const showTooltip = ref(false);
-
-const truncateDescription = (description: string, maxLength: number) => {
-    return description.length > maxLength ? description.slice(0, maxLength) + '...' : description;
-};
 
 const toggleTooltip = () => {
     showTooltip.value = !showTooltip.value;
 };
 
 const addToFavorites = () => {
-    console.log('Agregar a favoritos:', beer.value.name);
+    console.log('Agregar a favoritos:', brewery.value.name);
 };
 </script>
 
 <style scoped lang="scss">
 @import '../assets/cardbeer.scss';
-
 </style>
--- RebelBrews/src/views/BeersView.vue
+++ RebelBrews/src/views/BeersView.vue
@@ -1,144 +1,125 @@
-<script setup lang="ts">
-import { ref, onMounted } from 'vue';
-import axios from 'axios';
-import CardBeerComponent from '../components/CardBeerComponent.vue';
-import FilterBeerComponent from '@/components/FilterBeerComponent.vue';
-import type { Beer } from '@/types/Beer'; 
-
-const allBeers = ref<Beer[]>([]); 
-const currentPage = ref(1);
-const totalPages = ref(10); 
-const filteredBeers = ref<Beer[]>([]); 
-
-const fetchBeers = async (page: number) => {
-    try {
-        const response = await axios.get<Beer[]>(`https://api.punkapi.com/v2/beers?page=${page}&per_page=12`);
-        console.log(response.data); 
-        const newBeers = response.data;
-        allBeers.value = [...allBeers.value, ...newBeers];
-        updateFilteredBeers(); 
-    } catch (error) {
-        console.error('Error fetching beer data:', error);
-    }
-};
-
-onMounted(() => {
-    fetchBeers(currentPage.value);
-});
-
-const updateFilteredBeers = () => {
-    const startIndex = (currentPage.value - 1) * 12;
-    const endIndex = startIndex + 12;
-    filteredBeers.value = allBeers.value.slice(startIndex, endIndex);
-};
-
-const handleSearch = (searchTerm: string) => {
-    filteredBeers.value = allBeers.value.filter(beer =>
-        beer.name.toLowerCase().includes(searchTerm.toLowerCase())
-    );
-};
-
-const handleSearchAbv = (abv: number) => {
-    filteredBeers.value = allBeers.value.filter(beer =>
-        beer.abv === abv
-    );
-};
-
-const handleSearchIbu = (ibu: number) => {
-    filteredBeers.value = allBeers.value.filter(beer =>
-        beer.ibu === ibu
-    );
-};
-
-const resetSearch = () => {
-    filteredBeers.value = allBeers.value; 
-};
-
-const scrollToTop = () => {
-    window.scrollTo({
-        top: 0,
-        behavior: 'smooth'
-    });
-};
-
-const nextPage = () => {
-    if (currentPage.value < totalPages.value) {
-        currentPage.value++;
-        fetchBeers(currentPage.value); 
-        updateFilteredBeers(); 
-        scrollToTop(); // Llama a scrollToTop
-    }
-};
-
-const prevPage = () => {
-    if (currentPage.value > 1) {
-        currentPage.value--;
-        fetchBeers(currentPage.value); 
-        updateFilteredBeers(); 
-        scrollToTop(); // Llama a scrollToTop
-    }
-};
-</script>
-
 <template>
-   <div class="filterBeers">
-      <FilterBeerComponent @search="handleSearch" @reset="resetSearch" @searchAbv="handleSearchAbv" @searchIbu="handleSearchIbu" />
-   </div>
-   <div class="titleBeers">
-      <h1>LAS CERVEZAS MÁS REBELDES</h1>
-   </div>
-   <div class="beers-container">
-     <CardBeerComponent v-for="beer in filteredBeers" :key="beer.id" :beer="beer" />
-   </div>
-   <div class="pagination">
-     <button @click="prevPage" :disabled="currentPage === 1">Anterior</button>
-     <span>Página {{ currentPage }} de {{ totalPages }}</span>
-     <button @click="nextPage" :disabled="currentPage === totalPages">Siguiente</button>
-   </div>
- </template>
- 
-
-<style scoped lang="scss">
-
-.filterBeers{
-   margin: 2rem 0;
-}
-
-.titleBeers{
-   margin: 1rem 0;
-   text-align: center;
-}
-
-.beers-container {
- display: grid;
- grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
- grid-gap: 20px;
- padding: 20px;
-}
-
-.pagination {
- display: flex;
- justify-content: center;
- margin-top: 20px;
-}
-
-.pagination button {
-   background-color: #262425;
-   color: aliceblue;
-   border-radius: 10px;
-   transition: transform 0.3s ease-in-out;
-   padding: 0.5rem 2rem;
-   margin: 2rem;
-
- &:hover {
-   background-color: #F7BE37; 
-   color: #000; 
-   transform: scale(1.05);
- }
-
- &:disabled {
-    background-color: #ccc;
-    cursor: not-allowed;
- }
-}
-</style>
+    <div class="beers-container">
+       <FilterBeerComponent @search="handleSearch" @reset="resetSearch" />
+       <div v-for="brewery in filteredBreweries" :key="brewery.id">
+         <CardBeerComponent :brewery="brewery" />
+       </div>
+       <div class="pagination">
+         <button @click="prevPage" :disabled="currentPage === 1">Anterior</button>
+         <button @click="nextPage" :disabled="currentPage === totalPages">Siguiente</button>
+       </div>
+    </div>
+   </template>
+   
+   <script setup lang="ts">
+   import { ref, onMounted } from 'vue';
+   import axios from 'axios';
+   import CardBeerComponent from '../components/CardBeerComponent.vue';
+   import FilterBeerComponent from '@/components/FilterBeerComponent.vue';
+   
+   const allBreweries = ref<Brewery[]>([]); 
+   const currentPage = ref(1);
+   const totalPages = ref(10); 
+   const filteredBreweries = ref<Brewery[]>([]); 
+   
+   const fetchBreweries = async (page: number) => {
+       try {
+           const response = await axios.get(`https://api.openbrewerydb.org/breweries?page=${page}&per_page=12`);
+           console.log(response.data); 
+           const newBreweries = response.data;
+           allBreweries.value = [...allBreweries.value, ...newBreweries];
+           updateFilteredBreweries(); 
+       } catch (error) {
+           console.error('Error fetching brewery data:', error);
+       }
+   };
+   
+   onMounted(() => {
+       fetchBreweries(currentPage.value);
+   });
+   
+   const updateFilteredBreweries = () => {
+       const startIndex = (currentPage.value - 1) * 12;
+       const endIndex = startIndex + 12;
+       filteredBreweries.value = allBreweries.value.slice(startIndex, endIndex);
+   };
+   
+   const handleSearch = (searchTerm: string) => {
+       filteredBreweries.value = allBreweries.value.filter(brewery =>
+           brewery.name.toLowerCase().includes(searchTerm.toLowerCase())
+       );
+   };
+   
+   const resetSearch = () => {
+       filteredBreweries.value = allBreweries.value; 
+   };
+   
+   const scrollToTop = () => {
+       window.scrollTo({
+           top: 0,
+           behavior: 'smooth'
+       });
+   };
+   
+   const nextPage = () => {
+       if (currentPage.value < totalPages.value) {
+           currentPage.value++;
+           fetchBreweries(currentPage.value); 
+           updateFilteredBreweries(); 
+           scrollToTop(); // Llama a scrollToTop
+       }
+   };
+   
+   const prevPage = () => {
+       if (currentPage.value > 1) {
+           currentPage.value--;
+           fetchBreweries(currentPage.value); 
+           updateFilteredBreweries(); 
+           scrollToTop(); // Llama a scrollToTop
+       }
+   };
+   </script>
+   
+   <style scoped lang="scss">
+   .filterBeers{
+      margin: 2rem 0;
+   }
+   
+   .titleBeers{
+      margin: 1rem 0;
+      text-align: center;
+   }
+   
+   .beers-container {
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+    grid-gap: 20px;
+    padding: 20px;
+   }
+   
+   .pagination {
+    display: flex;
+    justify-content: center;
+    margin-top: 20px;
+   }
+   
+   .pagination button {
+      background-color: #262425;
+      color: aliceblue;
+      border-radius: 10px;
+      transition: transform 0.3s ease-in-out;
+      padding: 0.5rem 2rem;
+      margin: 2rem;
+   
+    &:hover {
+      background-color: #F7BE37; 
+      color: #000; 
+      transform: scale(1.05);
+    }
+   
+    &:disabled {
+       background-color: #ccc;
+       cursor: not-allowed;
+    }
+   }
+   </style>
